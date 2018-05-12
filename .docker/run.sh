d60cf8193fabc9dfaa06660b77d3d5e4e7b56eb7
GoForBroke1006 <go.for.broke1006@gmail.com>
2018-05-12 19:17:30 +0300

    Realize base logic

--- .docker/run.sh
+++ .docker/run.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
 
-#docker-compose build
-
-docker-compose down
+docker-compose build
 docker-compose up -d
 
 sleep 10 # wait when postgres warm up
 
 docker-compose exec db createdb lepp_demo || true
 
-#docker-compose exec fpm composer install
+docker-compose exec fpm composer install
 
 docker-compose exec fpm php bin/console doctrine:migrations:migrate --no-interaction
-#docker-compose exec fpm yarn run encore dev
+docker-compose exec fpm yarn run encore production
